8f9aa38c02b0781a0399ce0d622e17eeae64ed81
Owen <owencompher@outlook.com>
2022-04-06 11:39:21 -0400

    moved cycle colors function to external script

--- link-color.js
+++ link-color.js
@@ -8,6 +8,23 @@ const colors = [
 if(document.cookie.indexOf("color")==-1){
     document.cookie = `color=0; path=/; domain=owencompher.me`
 }
-const color = document.cookie.substr(document.cookie.indexOf("color")+6, 1)
+let colori = document.cookie.substr(document.cookie.indexOf("color")+6, 1)
+let color = colors[colori]
+if(!color) color = colors[0];
 document.getElementById('link-color').innerHTML =
-    `.link {color: ${colors[color]}}`
+    `.link {color: ${color}}`
+
+function cycleColors() {
+    var index = document.cookie.indexOf("color");
+    if (index == -1) {
+        document.cookie = `color=0; path=/; domain=owencompher.me`
+        var index = document.cookie.indexOf("color");
+    }
+    if (parseInt(document.cookie.substr(index + 6, 1)) == 4) document.cookie = `color=0; path=/; domain=owencompher.me`
+    else document.cookie = `color=${parseInt(document.cookie.substr(index + 6, 1)) + 1}; path=/; domain=owencompher.me`
+    const colori = document.cookie.substr(index + 6, 1)
+    let color = colors[colori]
+    if (!color) color = colors[0];
+    document.getElementById('link-color').innerHTML =
+        `.link {color: ${color}}`
+}
